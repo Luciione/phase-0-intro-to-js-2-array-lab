8d013fb804a0348637c815d847ed88f4242cb0ec
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-22 10:55:10 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(name) {
+  var newCats = [...cats, name];
+  return newCats;
+}
+
+function prependCat(name) {
+  var newCats = [name, ...cats];
+  return newCats;
+}
+
+function removeLastCat() {
+  var newCats = cats.slice(0, -1);
+  return newCats;
+}
+
+function removeFirstCat() {
+  var newCats = cats.slice(1);
+  return newCats;
+}
